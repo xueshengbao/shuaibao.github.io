44ca3e01b0d1c29182956adc366b5b74e5af5f08
xueshengbao <851516564@qq.com>
2016-10-29 13:18:10 +0800

    首次上传

---
+++ js/introduction.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+	$('#horizontalTab').easyResponsiveTabs({
+		type: 'default', //Types: default, vertical, accordion           
+		width: 'auto', //auto or any width like 600px
+		fit: true   // 100% fit in a container
+	});
+});		
+jQuery(function($) {
+	$(".swipebox").swipebox();
+});
+addEventListener("load", function() { 			setTimeout(hideURLbar, 0);
+}, false); 
+function hideURLbar(){ 
+	window.scrollTo(0,1); 
+}
